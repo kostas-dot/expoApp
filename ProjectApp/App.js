bfdea08087892ec2cea908b86087d02098a67c65
kostas-dot <126763090+kostas-dot@users.noreply.github.com>
2025-05-31 20:04:35 +0300

    Create App.js

---
+++ ProjectApp/App.js
@@ -0,0 +1,28 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import RestaurantsListScreen from './screens/RestaurantsListScreen';
+import ReservationFormScreen from './screens/ReservationFormScreen';
+import HomePage from './screens/HomePage';
+import AuthLoadingScreen from './screens/AuthLoadingScreen';
+import EditProfileScreen from './screens/EditProfileScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Restaurants" component={RestaurantsListScreen} />
+        <Stack.Screen name="ReservationForm" component={ReservationFormScreen} />
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="AuthLoading" component={AuthLoadingScreen} />
+        <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
