0cac1ee585b47f242f22f3166e28b6c9b678f1f6
kostas-dot <126763090+kostas-dot@users.noreply.github.com>
2025-05-31 20:07:32 +0300

    Delete NodeJsApplication12 directory

--- NodeJsApplication12/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/authRoutes.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/login', authController.login);
-router.post('/register', authController.register);
-
-module.exports = router;
